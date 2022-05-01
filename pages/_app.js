fa23d32177e6c7eed320c395fcd7067c67a1845e
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2022-05-01 18:10:03 -0300

    add styles

--- pages/_app.js
+++ pages/_app.js
@@ -13,9 +13,11 @@ function MyApp({ Component, pageProps }) {
 
 const GlobalStyle = createGlobalStyle`
   * {
+    @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;700&display=swap');
     box-sizing: border-box;
     margin: 0;
     padding: 0;
+    font-family: 'Montserrat', sans-serif;
   }
 `;
 
