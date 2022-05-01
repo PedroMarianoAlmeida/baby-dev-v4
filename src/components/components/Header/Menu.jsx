7ff35d624d25ed5ee035553ee985bab587966efb
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2022-05-01 17:01:32 -0300

    outro nome

--- src/components/components/Header/Menu.jsx
+++ src/components/components/Header/Menu.jsx
@@ -1,15 +1,28 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 const Menu = () => {
-    return (
-        <>
-        <Hamburguer src="images/menu-hamburguer.svg" />
-        </>
-      );
-}
+  const [showMenu, setShowMenu] = useState(false);
+
+  return (
+    <>
+      <Hamburguer src="images/menu-hamburguer.svg" />
+      <MenuGroup>
+        <ul></ul>
+      </MenuGroup>
+    </>
+  );
+};
 
 const Hamburguer = styled.img`
-    height: 18px;
-`
- 
+  height: 18px;
+`;
+
+const MenuGroup = styled.nav`
+  padding: 6px 0px;
+  background: #e9e9e9;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.31);
+  border-radius: 5px;
+`;
+
 export default Menu;
