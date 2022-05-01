f88533946dab43da22b76af7e21c22511893936d
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2022-05-01 18:46:29 -0300

    menu implemented (missing the links)

--- src/components/components/Header/Menu.jsx
+++ src/components/components/Header/Menu.jsx
@@ -13,10 +13,20 @@ const MenuWrapper = () => {
       <Hamburguer src="images/menu-hamburguer.svg" onClick={handleMenuClick} />
       <MenuGroup show={showMenu}>
         <ul>
-          <li>Home 🏠 </li>
-          <li>Busca 🔍</li>
-          <li>Parceiros 🤝</li>
-          <li>Contato ✉️</li>
+          <li>
+            Home <span>🏠</span>{" "}
+          </li>
+          <li>
+            Busca <span>🔍</span>
+          </li>
+          <li>
+            Parceiros <span>🤝</span>
+          </li>
+          <li>
+            Contato <span>✉️</span>
+          </li>
+          <li className="divider" />
+          <li className="login">Login</li>
         </ul>
       </MenuGroup>
     </Menu>
@@ -37,6 +47,7 @@ const MenuGroup = styled.nav`
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.31);
   border-radius: 5px;
   display: ${(props) => (props.show ? "unset" : "none")};
+  width: 130px;
 
   position: absolute;
   top: 30px;
@@ -44,15 +55,41 @@ const MenuGroup = styled.nav`
 
   ul {
     list-style: none;
-    padding: 22px 0;
-    width: 112px;
+    padding: 12px 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 100%;
 
     li {
       padding: 6px 22px;
+      font-style: normal;
+      font-weight: 600;
+      font-size: 12px;
+      line-height: 15px;
+      color: #202020;
+      cursor: pointer;
+      text-align: center;
+
+      span {
+        margin-left: 8px;
+      }
 
       :hover {
         background-color: #d7225e;
+        color: #e9e9e9;
       }
+    }
+
+    .login {
+      text-align: right;
+    }
+
+    .divider{
+        height: 0;
+        padding: 0;
+        border: 1px solid #CCCACA;
+        margin: 8px 0;
     }
   }
 `;
