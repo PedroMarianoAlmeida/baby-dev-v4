9894f8170d20133936172a66db6ced31c586884b
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2022-05-01 16:35:33 -0300

    reset CSS

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
-import Header from "../src/components/components/Header";
+import HeaderWrapper from "../src/components/components/Header";
+import { createGlobalStyle } from "styled-components";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
-      <Header />
+      <GlobalStyle />
+      <HeaderWrapper />
       <Component {...pageProps} />
     </>
   );
 }
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 export default MyApp;
---
+++ src/components/components/Header/index.jsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const HeaderWrapper = () => {
+  return (
+    <Header>
+      <p>Header</p>
+    </Header>
+  );
+};
+
+const Header = styled.header`
+  height: 54px;
+  background: #210037;
+`;
+
+export default HeaderWrapper;
