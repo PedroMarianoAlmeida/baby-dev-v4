8a4a8ba6591b2240e66798209fed15b9eb95366e
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2022-05-01 17:57:52 -0300

    starting menu

--- src/components/components/Header/Menu.jsx
+++ src/components/components/Header/Menu.jsx
@@ -4,15 +4,19 @@ import { useState } from "react";
 const MenuWrapper = () => {
   const [showMenu, setShowMenu] = useState(false);
 
+  const handleMenuClick = () => {
+    setShowMenu((prevState) => !prevState);
+  };
+
   return (
     <Menu>
-      <Hamburguer src="images/menu-hamburguer.svg" />
-      <MenuGroup>
+      <Hamburguer src="images/menu-hamburguer.svg" onClick={handleMenuClick} />
+      <MenuGroup show={showMenu}>
         <ul>
-          <li>Home</li>
-          <li>Busca</li>
-          <li>Parceiros</li>
-          <li>Contato</li>
+          <li>Home 🏠 </li>
+          <li>Busca 🔍</li>
+          <li>Parceiros 🤝</li>
+          <li>Contato ✉️</li>
         </ul>
       </MenuGroup>
     </Menu>
@@ -32,9 +36,25 @@ const MenuGroup = styled.nav`
   background: #e9e9e9;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.31);
   border-radius: 5px;
+  display: ${(props) => (props.show ? "unset" : "none")};
 
   position: absolute;
   top: 30px;
+  right: 0;
+
+  ul {
+    list-style: none;
+    padding: 22px 0;
+    width: 112px;
+
+    li {
+      padding: 6px 22px;
+
+      :hover {
+        background-color: #d7225e;
+      }
+    }
+  }
 `;
 
 export default MenuWrapper;
