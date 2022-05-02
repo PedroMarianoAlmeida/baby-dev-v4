34d20f0fda009a5da0f557072c19165a12da3dd2
Pedro Almeida <pedroalmeidaprogrammer@gmail.com>
2022-05-01 21:39:44 -0300

    all circles created

--- src/components/components/HomePage/Spots.jsx
+++ src/components/components/HomePage/Spots.jsx
@@ -7,11 +7,35 @@ const Spots = () => {
       <Avatar size="150px" src={"images/person1.png"} />
       <Avatar size="150px" src={"images/person2.png"} />
       <Avatar size="150px" src={"images/person3.png"} />
-      
+      <Circunference color={"#d7225e"} />
+      <Circunference color={"#FFEC43"} />
+      <Circunference color={"#FFEC43"} />
+      <BigCircle />
+      <BigCircle />
+      <SmallCircle />
     </Container>
   );
 };
 
 const Container = styled.div``;
+const Circunference = styled.div`
+  width: 150px;
+  height: 150px;
+  border: 1px solid ${(props) => props.color};
+  box-sizing: border-box;
+  border-radius: 300px;
+`;
+const BigCircle = styled.div`
+  width: 25px;
+  height: 25px;
+  background: #d7225e;
+  border-radius: 75px;
+`;
 
+const SmallCircle = styled.div`
+  width: 14px;
+  height: 14px;
+  background: #ffec43;
+  border-radius: 75px;
+`;
 export default Spots;
